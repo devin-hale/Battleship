9dea7e62dd8f4b08f633f8eb9791e5a69da20072
dwhale2 <devinwaynehale@gmail.com>
2023-07-05 16:31:37 -0500

    Grid renderer functioning.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css';
+import "./style.css";
+import { renderAI, renderPlayer } from "./rendering/renderpage";
+import gameboard from "./gameboard";
+
+let aiBoard = gameboard("AI");
+let playerBoard = gameboard("Player");
+
+renderAI(aiBoard);
+renderPlayer(playerBoard);
--- src/rendering/renderpage.js
+++ src/rendering/renderpage.js
@@ -1,1 +1,33 @@
-const renderPage = () => {};
+const renderAI = (board) => {
+  let gameDiv = document.getElementById("game");
+
+  let aiDiv = document.createElement("div");
+  aiDiv.classList = "aiDiv";
+
+  board.board.forEach((square) => {
+    let gameSquare = document.createElement("div");
+    gameSquare.id = `AI:${square.squareID}`;
+    gameSquare.classList = "coordSquare";
+    aiDiv.appendChild(gameSquare);
+  });
+
+  gameDiv.appendChild(aiDiv);
+};
+
+const renderPlayer = (board) => {
+  let gameDiv = document.getElementById("game");
+
+  let aiDiv = document.createElement("div");
+  aiDiv.classList = "playerDiv";
+
+  board.board.forEach((square) => {
+    let gameSquare = document.createElement("div");
+    gameSquare.id = `P:${square.squareID}`;
+    gameSquare.classList = "coordSquare";
+    aiDiv.appendChild(gameSquare);
+  });
+
+  gameDiv.appendChild(aiDiv);
+};
+
+export { renderAI, renderPlayer };
